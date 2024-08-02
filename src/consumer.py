f62d6ef09218f898b1823f348555ffad3449ba11
Mina unal <minaaunall@gmail.com>
2024-08-02 18:20:56 +0300

    qms pushed

---
+++ src/consumer.py
@@ -0,0 +1,22 @@
+import pika
+
+def callback(ch, method, properties, body):
+    print(f" [x] Received {body}")
+
+def main(queue_name):
+    credentials = pika.PlainCredentials('user', '12345')
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters('localhost', 5672, '/', credentials)
+    )
+    channel = connection.channel()
+
+    channel.queue_declare(queue=queue_name, durable=True)
+
+    channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
+
+    print(f" [*] Waiting for messages in queue '{queue_name}'. To exit press CTRL+C")
+    channel.start_consuming()
+
+if __name__ == "__main__":
+    queue_name = input("Queue name: ")
+    main(queue_name)
---
+++ src/producer.py
@@ -0,0 +1,20 @@
+import pika
+
+def main(exchange, routing_key, message):
+    credentials = pika.PlainCredentials('user', '12345')
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters('localhost', 5672, '/', credentials)
+    )
+    channel = connection.channel()
+
+    #channel.exchange_declare(exchange=exchange, exchange_type='direct', durable=True)
+
+    channel.basic_publish(exchange=exchange, routing_key=routing_key, body=message)
+    print(f" [x] Sent '{message}' to exchange '{exchange}' with routing key '{routing_key}'")
+    connection.close()
+
+if __name__ == "__main__":
+    exchange = input("Exchange name: ")
+    routing_key = input("Routing key: ")
+    message = input("Message: ")
+    main(exchange, routing_key, message)
